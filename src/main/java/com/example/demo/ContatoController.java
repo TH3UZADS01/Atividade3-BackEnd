06a49eddd039ee003797cc65829a7fb856b9e53c
TH3UZADS01 <matheuspiresdeoliveira903@gmail.com>
2026-03-24 20:37:24 -0300

    Aula para elaborar mais

--- src/main/java/com/example/demo/ContatoController.java
+++ src/main/java/com/example/demo/ContatoController.java
@@ -1,28 +1,33 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
+/*import com.example.demo.Contato;*/
 
 @RestController
 @RequestMapping("/contato")
 public class ContatoController {
+    List<Contato> contatos = new ArrayList<Contato>();
+    
+    @PostMapping
+    public String criar(@RequestBody Contato contato) {
+        contatos.add(contato);
+        return "Contato criado com sucesso!";
+    }
 
     @GetMapping
-    public String listar() {
-        return "GET funcionando!";
+    public List<Contato> listar() {
+        return contatos;
     }
-
-    @PostMapping
-    public String criar() {
-        return "POST funcionando!";
-    }
+    
 
     @PutMapping
     public String atualizar() {
-        return "PUT funcionando!";
+        return "Contato atualizado com sucesso!";
     }
 
     @DeleteMapping
     public String deletar() {
-        return "DELETE funcionando!";
+        return "Contato deletado com sucesso!";
     }
 }
